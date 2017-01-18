d59d81f49d8d0db11eb67db9961726b6ded332a8
Chris Griffing <cmgriffing@gmail.com>
2017-01-18 13:58:05 -0800

    Add Dockerfile, log ip, and add build command to npm scripts

--- server/index.js
+++ server/index.js
@@ -1,5 +1,29 @@
 'use strict';
 
+// list network interfaces
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
+
 require('node-jsx').install({extension: '.jsx', harmony: true}); // parses JSX and enables ES6/7 transpiling
 
 var express        = require('express');
